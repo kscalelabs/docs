b3cc5cdef51dfb7a9702c39d267a3c0d86ac32d5
Benjamin Bolte <ben@bolte.cc>
2025-09-23 15:56:26 -0700

    correct url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kscalelabs.github.io',
+  url: 'https://docs.kscale.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
